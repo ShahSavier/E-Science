becc815ad4c4ed68bcc07858120d142d6571a52c
ShahSavier <alif.shahkimi99@gmail.com>
2020-02-25 22:53:11 +0800

    --- Auto-Git Commit ---

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\X556u\Desktop\b\Apps\sains"
+export "FLUTTER_APPLICATION_PATH=C:\Users\X556u\Desktop\My_Code\Flutter_Application\SMKAY_E-SAINS\sains"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
